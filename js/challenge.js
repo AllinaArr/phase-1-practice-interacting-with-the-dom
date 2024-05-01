1a49ae3e08cb8819c0515e4b0dfbcc4e8da7b2e8
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2024-05-01 16:17:39 -0400

    submit and leave comment done

--- js/challenge.js
+++ js/challenge.js
@@ -2,9 +2,16 @@ const plusBtn = document.querySelector("#plus");
 const minusBtn = document.querySelector("#minus");
 const heartBtn = document.querySelector("#heart");
 const pauseBtn = document.querySelector("#pause");
+const likesUl = document.querySelector(".likes");
+const submitBtn = document.querySelector("#submit");
+const comment = document.querySelector(".comments");
+const commentForm = document.querySelector("#comment-form");
+const commentInput = document.querySelector("#comment-input");
 
 var counterLocation = document.querySelector("#counter");
 var counterValue = parseInt(counterLocation.textContent);
+
+//
 
 // Define the increaseCounter function
 function increaseCounter() {
@@ -25,4 +32,23 @@ minusBtn.addEventListener("click", () => {
     counterValue -= 1;
     counterLocation.textContent = counterValue;
   }
+});
+
+var timesCount = {};
+
+heartBtn.addEventListener("click", () => {
+  var count = timesCount[counterValue];
+  count = (timesCount[counterValue] || 0) + 1;
+
+  var likesLi = document.createElement("li");
+  likesLi.textContent = `${counterValue} has been liked  ${count}  times `;
+  likesUl.append(likesLi);
+});
+
+commentForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  var parag = document.createElement("p");
+  parag.textContent = commentInput.value;
+  comment.appendChild(parag);
 });
