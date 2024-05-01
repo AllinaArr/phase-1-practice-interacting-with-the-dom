79de91c374176212c8589901d334caf40517195f
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2024-05-01 14:18:38 -0400

    counter automatically implemented

--- js/challenge.js
+++ js/challenge.js
@@ -3,16 +3,24 @@ const minusBtn = document.querySelector("#minus");
 const heartBtn = document.querySelector("#heart");
 const pauseBtn = document.querySelector("#pause");
 
+var counterLocation = document.querySelector("#counter");
+var counterValue = parseInt(counterLocation.textContent);
+
+// Define the increaseCounter function
+function increaseCounter() {
+  counterValue += 1; // Increment the counter value by 1
+  counterLocation.textContent = counterValue; // Update the counter display
+}
+
+// Set an interval to call the increaseCounter function every second
+var interval = setInterval(increaseCounter, 1000);
+
 plusBtn.addEventListener("click", function () {
-  var counterLocation = document.querySelector("#counter");
-  var counterValue = parseInt(counterLocation.textContent);
   counterValue += 1;
   counterLocation.textContent = counterValue;
 });
 
 minusBtn.addEventListener("click", () => {
-  var counterLocation = document.querySelector("#counter");
-  var counterValue = parseInt(counterLocation.textContent);
   if (counterValue > 0) {
     counterValue -= 1;
     counterLocation.textContent = counterValue;
